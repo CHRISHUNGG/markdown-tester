f808d99e1b163d9f45ae246eb952b0851fefe64b
hungchihsueh <84211143+hungchihsueh@users.noreply.github.com>
2023-12-22 18:08:12 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,24 @@ import { useState } from "react";
 export default function Home() {
 	const [string, setString] = useState<string>("");
 	return (
-		<div className=" w-dvw h-dvh bg-gray-200 flex flex-col justify-center items-center gap-4">
-			<input
-				className="w-1/2 p-1 rounded border shadow-inner bg-white"
-				onChange={(e) => setString(e.target.value)}
-				type="text"
-				name=""
-				id=""
-			/>
+		<div className=" w-dvw h-dvh bg-gray-200 flex flex-col justify-center items-center gap-4 bg-gradient-to-br from-[#824FFB] to-[#3B60EC] bg-clip-text text-transparent">
+			<h1 className="font-bold text-2xl drop-shadow-md">MARKDOWN TESTER</h1>
+			<div className="flex w-1/2 justify-center gap-3 items-center">
+				<input
+					value={string}
+					className="flex-1 p-1 rounded border shadow-inner gap-3 bg-white"
+					onChange={(e) => setString(e.target.value)}
+					type="text"
+					name=""
+					id=""
+				/>
+				<button
+					className="py-1 px-3 rounded bg-black text-white font-medium"
+					onClick={() => setString("")}>
+					clear
+				</button>
+			</div>
+
 			<div className="p-1 border rounded bg-white w-1/2 aspect-video">
 				<MdxDisplay mdxString={string.replaceAll("\\\\", "\\")} />
 			</div>
