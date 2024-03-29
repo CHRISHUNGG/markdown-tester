65769ecde39baee9e4e3e8b663d6c8d7a896cbd6
hungchihsueh <84211143+hungchihsueh@users.noreply.github.com>
2024-03-29 13:39:06 +0800

    fix: fix

--- components/mdx/MdxDisplay.tsx
+++ components/mdx/MdxDisplay.tsx
@@ -1,7 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
 
-import { useState, useEffect, Fragment } from "react";
+// import "github-markdown-css/github-markdown.css";
+// import "github-markdown-css/github-markdown-dark.css";
+// import "github-markdown-css/github-markdown-light.css";
 // mdx
 import "katex/dist/katex.min.css";
 import { compile, run } from "@mdx-js/mdx";
@@ -9,37 +11,77 @@ import * as runtime from "react/jsx-runtime";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 import remarkGfm from "remark-gfm";
+import rehypeMathjax from "rehype-mathjax";
 type Props = {
 	mdxString: string;
+	setIsMdxFinishedRendered?: React.Dispatch<React.SetStateAction<boolean>>;
 };
+/**
+ * Replaces newline characters outside of dollar expressions with the HTML <br/> tag.
+ * @param text - The input text that may contain newline characters.
+ * @returns A string with newline characters outside of dollar expressions replaced with the HTML <br/> tag.
+ */
+function replaceNewlinesOutsideDollarExpressions(text: string): string {
+	let insideDollarExpression = false;
 
-const MdxDisplay = ({ mdxString }: Props) => {
+	//  check wether the text includes dollar sign
+	return String(text)
+		.split("$")
+		.map((segment, index) => {
+			console.log("segment", segment);
+			insideDollarExpression = index % 2 === 1;
+			return insideDollarExpression
+				? segment
+				: segment.replace(/\\n/g, "<br />");
+		})
+		.join("$");
+}
+const MdxDisplay = ({ mdxString, setIsMdxFinishedRendered }: Props) => {
 	// mdx
 	const [mdxModule, setMdxModule] = useState<any>();
 	const Content = mdxModule ? mdxModule.default : Fragment;
+	let checkString = mdxString ? mdxString : "";
 	useEffect(
 		function () {
 			(async function () {
+				const reg = new RegExp(/(?<!\|)\\n(?! *\|)/g);
+
+				let str: string = replaceNewlinesOutsideDollarExpressions(checkString)
+					? replaceNewlinesOutsideDollarExpressions(checkString)
+					: checkString.replace(reg, " <br /> ");
+
 				const code = String(
-					await compile(
-						`${mdxString
-							.replaceAll("\\n", "\n")
-							.replaceAll(/(?<!\|)\n(?!|)/g, "<br/>")}`,
-						{
-							outputFormat: "function-body",
-							rehypePlugins: [rehypeKatex],
-							remarkPlugins: [remarkMath, remarkGfm],
-						},
-					),
+					await compile(str, {
+						outputFormat: "function-body",
+						rehypePlugins: [rehypeKatex, rehypeMathjax],
+						remarkPlugins: [remarkMath, remarkGfm],
+					}),
 				);
+				// console.log("code and string", code, str);
 				setMdxModule(
-					await run(code, { ...runtime, baseUrl: import.meta.url, Fragment }),
+					await run(code, {
+						...(runtime as any),
+						baseUrl: import.meta.url,
+						Fragment,
+					}),
 				);
 			})();
 		},
 		[mdxString],
 	);
-	return <Content />;
+	useEffect(() => {
+		setIsMdxFinishedRendered && setIsMdxFinishedRendered(false);
+		if (Content !== Fragment) {
+			setIsMdxFinishedRendered && setIsMdxFinishedRendered(true);
+		} else {
+			setIsMdxFinishedRendered && setIsMdxFinishedRendered(false);
+		}
+	}, [mdxModule]);
+	return (
+		<div>
+			<Content />
+		</div>
+	);
 };
 
 export default MdxDisplay;
